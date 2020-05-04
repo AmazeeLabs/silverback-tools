6c0c1e6eb7f77baaac0a8ab8b9875efbd7c07c98
Philipp Melab <philipp.melab@gmail.com>
2020-05-04 12:43:54 +0200

    fix(tests): fixed typings of command test helper

--- src/test/command.ts
+++ src/test/command.ts
@@ -1,10 +1,10 @@
-import {command as oclifCommand} from '@oclif/test';
-import {Config} from '@oclif/test';
+import {command as oclifCommand} from '@oclif/test/lib/command';
+import {load} from '@oclif/config';
 import path from 'path';
 
 export const command = async (args: string[]): Promise<void> => {
   return oclifCommand(args).run({
-    config: await Config.load({root: path.resolve(__dirname, '../../')}),
+    config: await load({root: path.resolve(__dirname, '../../')}),
     expectation: '',
   });
 };
