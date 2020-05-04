2d996f075ccc1269907fe899e6c0fc73e7fbe4cd
Philipp Melab <philipp.melab@gmail.com>
2020-05-04 09:15:54 +0200

    feat: Output JSON if the target file extension is .json.

--- test/commands/yaml.test.ts
+++ test/commands/yaml.test.ts
@@ -2,16 +2,32 @@ import {expect, test} from '@oclif/test';
 import fs from 'fs';
 import sinon from 'sinon';
 
-describe('yaml', () => {
-  const fileName = 'test.yml';
-  const content = 'foo: bar';
+describe('write new yaml file', () => {
   const stub = sinon.stub();
 
   test
     .stub(fs, 'writeFileSync', stub)
     .stdout()
-    .command(['yaml', fileName, content])
-    .it('writes yaml to a file', () => {
-      expect(stub.calledWithExactly(fileName, content)).to.be.true;
+    .command(['yaml', 'test.yml', 'foo: bar\n']);
+
+  test
+    .stub(fs, 'writeFileSync', stub)
+    .stdout()
+    .command(['yaml', 'test.yaml', 'foo: bar'])
+    .it('writes YAML to a file with extension .yaml', () => {
+      expect(stub.calledWithExactly('test.yaml', 'foo: bar\n')).to.be.true;
+    });
+});
+
+describe('write json file', () => {
+  const stub = sinon.stub();
+  test
+    .stub(fs, 'writeFileSync', stub)
+    .stdout()
+    .command(['yaml', 'test.json', 'foo: bar'])
+    .it('writes formatted JSON to a file with extension .json', () => {
+      expect(stub.called).to.be.true;
+      expect(stub.getCall(0).args[0]).to.equal('test.json');
+      expect(stub.getCall(0).args[1]).to.equal('{\n  "foo": "bar"\n}\n');
     });
 });
