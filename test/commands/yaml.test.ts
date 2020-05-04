6d29fe5c590126a091f8abfbbef09cb515e9b55c
Philipp Melab <philipp.melab@gmail.com>
2020-05-04 11:04:10 +0200

    ref: Avoid @oclif/test and use more standard mocha/chai/sinon structure.
    Reasons:
    * Chained test calls don't play well with ui test runners.
    * Chained test calls cause performance problems in Webstorm.
    * More granular control over test behaviour.

--- test/commands/yaml.test.ts
+++ test/commands/yaml.test.ts
@@ -1,33 +1,34 @@
-import {expect, test} from '@oclif/test';
+import {expect} from 'chai';
 import fs from 'fs';
 import sinon from 'sinon';
+import {command} from '../../src/test/command';
 
-describe('write new yaml file', () => {
-  const stub = sinon.stub();
+describe('yaml', () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
 
-  test
-    .stub(fs, 'writeFileSync', stub)
-    .stdout()
-    .command(['yaml', 'test.yml', 'foo: bar\n']);
+  it('writes YAML to a file with extension .yml', async () => {
+    const stub = sinon.stub(fs, 'writeFileSync');
+    await command(['yaml', 'test.yml', 'foo: bar']);
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.getCall(0).args[0]).to.equal('test.yml');
+    expect(stub.getCall(0).args[1]).to.equal('foo: bar\n');
+  });
 
-  test
-    .stub(fs, 'writeFileSync', stub)
-    .stdout()
-    .command(['yaml', 'test.yaml', 'foo: bar'])
-    .it('writes YAML to a file with extension .yaml', () => {
-      expect(stub.calledWithExactly('test.yaml', 'foo: bar\n')).to.be.true;
-    });
-});
+  it('writes YAML to a file with extension .yaml', async () => {
+    const stub = sinon.stub(fs, 'writeFileSync');
+    await command(['yaml', 'test.yaml', 'foo: bar']);
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.getCall(0).args[0]).to.equal('test.yaml');
+    expect(stub.getCall(0).args[1]).to.equal('foo: bar\n');
+  });
 
-describe('write json file', () => {
-  const stub = sinon.stub();
-  test
-    .stub(fs, 'writeFileSync', stub)
-    .stdout()
-    .command(['yaml', 'test.json', 'foo: bar'])
-    .it('writes formatted JSON to a file with extension .json', () => {
-      expect(stub.called).to.be.true;
-      expect(stub.getCall(0).args[0]).to.equal('test.json');
-      expect(stub.getCall(0).args[1]).to.equal('{\n  "foo": "bar"\n}\n');
-    });
+  it('writes formatted JSON to a file with extension .json', async () => {
+    const stub = sinon.stub(fs, 'writeFileSync');
+    await command(['yaml', 'test.json', 'foo: bar']);
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.getCall(0).args[0]).to.equal('test.json');
+    expect(stub.getCall(0).args[1]).to.equal('{\n  "foo": "bar"\n}\n');
+  });
 });
