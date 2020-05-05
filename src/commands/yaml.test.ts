43d7988b39ce37e3231c16a3f6e8a488fc6b582b
Philipp Melab <philipp.melab@gmail.com>
2020-05-05 08:55:31 +0200

    refactor(tests): consolidated test process for yaml write operations

--- src/commands/yaml.test.ts
+++ src/commands/yaml.test.ts
@@ -5,17 +5,32 @@ import sinon from 'sinon';
 import Yaml from './yaml';
 import {stderr} from 'stdout-stderr';
 
+/**
+ * Test a single yaml write invocation.
+ *
+ * @param file
+ *   The file to be written to.
+ * @param content
+ *   The content to pass into the command.
+ * @param expected
+ *   The content expected to be written to file.
+ */
+const testWrite = async (file: string, content: string, expected: string) => {
+  const stub = sinon.stub(fs, 'writeFile');
+  await Yaml.run([file, content]);
+  expect(stub.calledOnce).to.be.true;
+  expect(stub.getCall(0).args[0]).to.equal(file);
+  expect(stub.getCall(0).args[1]).to.equal(expected);
+  return stub;
+};
+
 describe('yaml', () => {
   beforeEach(() => {
     sinon.restore();
   });
 
   it('writes YAML to a file with extension .yml', async () => {
-    const stub = sinon.stub(fs, 'writeFile');
-    await Yaml.run(['test.yml', 'foo: bar']);
-    expect(stub.calledOnce).to.be.true;
-    expect(stub.getCall(0).args[0]).to.equal('test.yml');
-    expect(stub.getCall(0).args[1]).to.equal('foo: bar\n');
+    await testWrite('test.yml', 'foo: bar', 'foo: bar\n');
   });
 
   it('raises an error on malformed yaml input', async () => {
@@ -26,19 +41,11 @@ describe('yaml', () => {
   });
 
   it('writes YAML to a file with extension .yaml', async () => {
-    const stub = sinon.stub(fs, 'writeFile');
-    await Yaml.run(['test.yaml', 'foo: bar']);
-    expect(stub.calledOnce).to.be.true;
-    expect(stub.getCall(0).args[0]).to.equal('test.yaml');
-    expect(stub.getCall(0).args[1]).to.equal('foo: bar\n');
+    await testWrite('test.yaml', 'foo: bar', 'foo: bar\n');
   });
 
   it('writes formatted JSON to a file with extension .json', async () => {
-    const stub = sinon.stub(fs, 'writeFile');
-    await Yaml.run(['test.json', 'foo: bar']);
-    expect(stub.calledOnce).to.be.true;
-    expect(stub.getCall(0).args[0]).to.equal('test.json');
-    expect(stub.getCall(0).args[1]).to.equal('{\n  "foo": "bar"\n}\n');
+    await testWrite('test.json', 'foo: bar', '{\n  "foo": "bar"\n}\n');
   });
 
   it('deep merges documents if the target file already exists', async () => {
